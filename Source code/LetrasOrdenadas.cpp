7b5e37232de2255632f03cddb6988917d1773e11
Marta Pastor Puente <marta.pastor@live.com>
2016-10-20 10:43:06 +0200

    Added correct source code for statement DG07. Already checked and correct.

--- Source code/LetrasOrdenadas.cpp
+++ Source code/LetrasOrdenadas.cpp
@@ -21,7 +21,7 @@ int main() {
 		cin >> testcase;
 	}
 
-	system("pause");
+	//system("pause");
 
 	return 0;
 }
@@ -30,7 +30,7 @@ bool letrasOrdenadas(string word) {
 	bool exists = true;
 
 	for (unsigned i = 0; i < word.length() - 1; i++) {
-		if(word.at(i) > word.at(i+1)) {
+		if(word.at(i) > word.at(i+1) || word.at(i) == word.at(i+1)) {
 			exists = false;
 		}
 	}
