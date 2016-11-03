e14ffb35f2d8d699bb4a4d5b39326efa41d5f6f2
Marta Pastor Puente <marta.pastor@live.com>
2016-11-03 18:41:26 +0100

    Solution to this problem is still not complete.

---
+++ src/Interpares.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int interpares(int v[], int n);
+int interimpares(int v[], int n);
+
+int main() {
+
+
+
+
+
+  return 0;
+}
+
+int interpares(int v[], int n) {
+
+
+}
+
+int interimpares(int v[], int n) {
+
+
+
+}
