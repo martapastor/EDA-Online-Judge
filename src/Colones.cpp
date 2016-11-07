f347f1a8be4abeba64096d9ae0ba48aae0e07edd
Marta Pastor Puente <marta.pastor@live.com>
2016-11-07 15:28:55 +0100

    Updated correct source code for problem A03.

--- src/Colones.cpp
+++ src/Colones.cpp
@@ -4,9 +4,11 @@ using namespace std;
 
 int colones(int v[], int n);
 
+const int MAX_ELEMS = 100000;
+
 int main() {
   int testcases, n;
-  int v[100000];
+  int *v = new int[MAX_ELEMS];
 
   cin >> testcases;
 
@@ -20,6 +22,8 @@ int main() {
     cout << colones(v, n) << endl;
   }
 
+  delete[] v;
+
   return 0;
 }
 
@@ -27,6 +31,7 @@ int colones(int v[], int n) {
   int num = 0;
   bool encontrado = false;
 
+  /*
   for (int i = 0; i < n; i++) {
     int j = i + 1;
     while (!encontrado && j < n) {
@@ -39,6 +44,18 @@ int colones(int v[], int n) {
 
     j = 0;
     encontrado = false;
+  }
+  */
+
+  int min = v[n-1];
+
+  for (int i = n - 2; i >= 0; i--) {
+    if (v[i] < min) {
+      min = v[i];
+    }
+    else if (v[i] > min) {
+      num++;
+    }
   }
 
   return num;
